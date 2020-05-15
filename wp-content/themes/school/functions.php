d022ba3919777ba42567f79cdefd6b42264abf42
waynellincoln <57865047+waynellincoln@users.noreply.github.com>
2020-05-15 18:26:43 -0400

    Convert Static Page to WordPress
    Layout html to header file; to footer file, etc
    Connect style page
    Connect images to file using function
    Link font awesome
    Link google font
    Link javascript

--- wp-content/themes/school/functions.php
+++ wp-content/themes/school/functions.php
@@ -2,7 +2,14 @@
     
     function btdc_school_files() {
         
+        wp_enqueue_script('javascrpt_file', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+        
+        wp_enqueue_style('google_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+        
+        wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+        
         wp_enqueue_style('btdc_main_styles', get_stylesheet_uri());
+        
     }
 
     add_action('wp_enqueue_scripts', 'btdc_school_files');
